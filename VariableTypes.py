df155571f33d6ff7bb0eab88c9f4b0d538271c19
DucVan <vnduc11@gmail.com>
2017-06-07 14:03:57 +0700

    String

---
+++ VariableTypes.py
@@ -0,0 +1,25 @@
+#Gan gia tri 
+a = 2; b = 2.1;c = "VND"
+print a
+print b
+print c
+#Gan gia tri don cho nhieu bien
+a = b = c = 5
+print a
+print b
+print c
+#Gan gia tri cho nhieu bien
+a,b,c = 5,10,15
+print a 
+print b
+print c
+#Chuyen doi kieu trong python
+var1 = 1
+var2 = 10
+
+#String
+myVar = "Đây là tôi -"
+myVar2 = "Tôi muốn học lập trình Python"
+myVar3 =   myVar + " " + myVar2
+
+print (myVar3) 

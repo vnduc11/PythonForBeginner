713105bfea398debf9bc1c1d8a199df6749ef495
DucVan <vnduc11@gmail.com>
2017-06-07 10:43:47 +0700

    thay doi ten file

---
+++ thamsodonglenh.py
@@ -0,0 +1,28 @@
+#Thut dong
+# x= True
+# if x==False:
+#     print ("True")
+# else:
+#     print ("False")
+#     print("Demo")
+
+#Lenh tren nhieu dong
+# a = "So 1" +\
+#     " So 2"
+# print (a)
+
+# days = ['Monday', 'Tuesday', 'Wednesday',
+#         'Thursday', 'Friday']
+# print(days)
+
+#Comment trong Python
+#Comment tren 1 dong #
+print "Hello Python" 
+#Comment tren nhieu dong
+print "Hello Python"
+"""Demo 
+Demo 2""" 
+
+import sys; x = "foo"; sys.stdout.write(x + '\n')
+
+

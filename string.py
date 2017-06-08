e430149890051d3d67178d3a370ed3559db19426
DucVan <vnduc11@gmail.com>
2017-06-08 14:30:04 +0700

    modify

---
+++ string.py
@@ -0,0 +1,16 @@
+var1 = 'Python Programmming'
+
+print var1[2]
+print var1[1:4]
+
+var2 = var1
+
+print "Update String: ", var1[:7] + 'Python'
+
+print 4*var1
+
+print "Ten toi la %s va toi nang %d kg" % ('Duc', 75)
+
+var1 = "toi la ai"
+
+print var1.upper()
